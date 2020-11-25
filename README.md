85b766fa06bcf5cd9d771574082ca980227c0cbd
wenbinai <208176146@qq.com>
2020-11-25 21:45:03 +0800

    20-11-25 [env] 完成SpringBoot基本环境搭建和README.md的创建

---
+++ README.md
@@ -0,0 +1,14 @@
+# my-blog
+## 2020-12-25
+#### 1. 完成基本环境搭建
+SpringBoot版本 2.2.11.RELEASE
+相关依赖
+ - jpa
+ - Lombok
+ - mysql
+ - spring security
+
+JDK版本: 1.8
+
+Maven: 3.6
+
---
+++ src/test/java/edu/nefu/myblog/MyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nefu.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
